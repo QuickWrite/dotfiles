febaf7b3447a0e2c38a77806c0ab687321218cd2
QuickWrite <quickwrite@users.noreply.github.com>
2025-12-10 21:58:39 +0100

    Ready set - basic system
    Create the basic system for dotfiles and a basic Hyprland system.
    Nothing fancy.

---
+++ .config/yazi/install_yazi.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# Install Cattpucchin themes
+ya pkg add yazi-rs/flavors:catppuccin-mocha
+
+# Install Smart Enter to not enter Vim for folders 
+ya pkg add yazi-rs/plugins:smart-enter
+
