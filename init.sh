615a08dab66e8be2a22ebfc1c62d65e605925a87
QuickWrite <git@quickwrite.net>
2026-07-17 15:01:23 +0200

    Add a simple init script that installs some programs

---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd "$(dirname $0)"
+
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+light_red=$(tput setaf 9)
+bold=$(tput bold)
+reset=$(tput sgr0)
+
+# Upgrading the system
+pacman -Syu --needed --noconfirm
+
+# Install Hyprland & stuff
+pacman -S hyprland kitty rofi waybar --needed --noconfirm
+
+# Install extra terminal apps
+pacman -S btop yazi eza vim mandoc man-pages --needed --noconfirm
+
